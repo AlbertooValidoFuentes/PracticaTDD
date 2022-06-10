58152ccd811a5c66281ae95d1feb220177646a40
Alberto Valido Fuentes <albertovalidofuentes2001@gmail.com>
2022-06-10 08:20:13 +0000

    Test verde segundo punto

--- Contador.py
+++ Contador.py
@@ -1,2 +1,6 @@
 class Contador:
-    pass
+    def __init__(self, valorLimite, valorInicial=0, valorIncremento=1):
+        self.contador = valorInicial
+        self.valorInicial = valorInicial
+        self.valorIncremento = valorIncremento
+        self.valorLimite = valorLimite

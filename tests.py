7771130614f498567ad7426b60d79a5b9c81a2ca
Alberto Valido Fuentes <albertovalidofuentes2001@gmail.com>
2022-06-10 09:15:00 +0000

    Test verde punto 3

--- tests.py
+++ tests.py
@@ -12,29 +12,29 @@ class TestContador(unittest.TestCase):
             Funcion que contiene el test para comprobar que el constructor recoge correctamente los parámetros. 
         '''
         contador = Contador(10, 2, 2)
-        self.assertEqual(contador.valorLimite, 10)
-        self.assertEqual(contador.valorInicial, 2)
-        self.assertEqual(contador.valorIncremento, 2)
+        self.assertEqual(contador.getValorLimite(), 10)
+        self.assertEqual(contador.getValorInicial(), 2)
+        self.assertEqual(contador.getValorIncremento(), 2)
     
     def test_valores_iniciales(self):
         '''
             Funcion que contiene el test para comprobar que el valor inicial y incremento es correcto. 
         '''
         contador = Contador(10)
-        self.assertEqual(contador.valorInicial, 0)
-        self.assertEqual(contador.valorIncremento, 1)
+        self.assertEqual(contador.getValorInicial(), 0)
+        self.assertEqual(contador.getValorIncremento(), 1)
         
     def test_valores_noCambian(self):
         '''
-            Funcion que contiene el test para comprobar que el valor inicial y incremento no cambian. 
+            Funcion que contiene el test para comprobar que el valor inicial, incremento y limite no cambian. 
         '''
         contador = Contador(10, 2, 2)
-        contador.valorInicial = 3
-        contador.valorIncremento = 3
-        contador.valorLimite = 3
-        self.assertEqual(contador.valorInicial, 2)
-        self.assertEqual(contador.valorIncremento, 2)
-        self.assertEqual(contador.valorLimite, 10)
+        contador.__valorInicial = 3
+        contador.__valorIncremento = 3
+        contador.__valorLimite = 3
+        self.assertEqual(contador.getValorInicial(), 2)
+        self.assertEqual(contador.getValorIncremento(), 2)
+        self.assertEqual(contador.getValorLimite(), 10 )
     
 if __name__ == '__main__':
     unittest.main()

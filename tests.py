15a5ff3c7629f7b2495569aacb5f61013878fb65
Alberto Valido Fuentes <albertovalidofuentes2001@gmail.com>
2022-06-10 08:18:03 +0000

    Test rojo primer punto

--- tests.py
+++ tests.py
@@ -3,7 +3,18 @@ from Contador import Contador
 
 class TestContador(unittest.TestCase):
     
-   pass
+    '''
+        Clase para hacer los test  de la clase Contador (Contador.py)
+    '''
+    
+    def test_constructor(self):
+        '''
+            Funcion que contiene el test para comprobar que el constructor recoge correctamente los parámetros. 
+        '''
+        contador = Contador(10, 2, 2)
+        self.assertEqual(contador.valorLimite, 10)
+        self.assertEqual(contador.valorInicial, 2)
+        self.assertEqual(contador.valorIncremento, 2)
     
     
 
